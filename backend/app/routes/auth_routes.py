1ccb0afa2ea7b5ef71c0dcd1935f07b465c438b9
Julieta <julieta_carmona_99@hotmail.com>
2026-07-27 18:08:11 -0300

    agrego ruta de login

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -7,3 +7,7 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 @auth_bp.route("/register", methods=["POST"])
 def registrar():
     return AuthController.registrar()
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    return AuthController.login()
