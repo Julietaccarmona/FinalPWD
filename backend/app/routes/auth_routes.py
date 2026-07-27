8b4c1170e82c71a45c04ab186e50df50504e7340
Julieta <julieta_carmona_99@hotmail.com>
2026-07-27 18:52:24 -0300

    Implemento y pruebo autenticación JWT y protección de rutas

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from app.controllers.auth_controller import AuthController
+from flask_jwt_extended import jwt_required
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -11,3 +12,8 @@ def registrar():
 @auth_bp.route("/login", methods=["POST"])
 def login():
     return AuthController.login()
+
+@auth_bp.route("/perfil", methods=["GET"])
+@jwt_required()
+def perfil():
+    return AuthController.perfil()
