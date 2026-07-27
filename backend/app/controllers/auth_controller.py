57241c663df2e77d49886fc30c100de60a8db6ed
Julieta <julieta_carmona_99@hotmail.com>
2026-07-26 22:18:17 -0300

    validación de registro en Auth_controller

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,2 +1,39 @@
+from flask import request, jsonify
+from app.services.auth_service import AuthService
+
+
 class AuthController:
-    pass
+
+    @staticmethod
+    def registrar():
+        datos = request.get_json()
+
+        nombre = datos.get("nombre")
+        email = datos.get("email")
+        password = datos.get("password")
+
+        if not nombre or not email or not password:
+            return jsonify({
+                "mensaje": "Todos los campos son obligatorios."
+            }), 400
+
+        usuario, error = AuthService.registrar_usuario(
+            nombre,
+            email,
+            password
+        )
+
+        if error:
+            return jsonify({
+                "mensaje": error
+            }), 400
+
+        return jsonify({
+            "mensaje": "Usuario registrado correctamente.",
+            "usuario": {
+                "id": usuario.id,
+                "nombre": usuario.nombre,
+                "email": usuario.email,
+                "rol": usuario.rol
+            }
+        }), 201
