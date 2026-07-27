9ae52e0ff641b5057ac506debd5c98651b547b2b
Julieta <julieta_carmona_99@hotmail.com>
2026-07-27 17:30:52 -0300

    registro ruta auth_routes

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,3 +1,9 @@
 from flask import Blueprint
+from app.controllers.auth_controller import AuthController
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@auth_bp.route("/register", methods=["POST"])
+def registrar():
+    return AuthController.registrar()
