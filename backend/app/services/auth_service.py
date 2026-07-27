59702ab088776c55c715571d193fb363ae83a908
Julieta <julieta_carmona_99@hotmail.com>
2026-07-26 21:53:05 -0300

    Registro de usuarios en AuthService

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,2 +1,26 @@
+from app.extensions import db
+from app.models.usuario import Usuario
+
+
 class AuthService:
-    pass
+
+    @staticmethod
+    def registrar_usuario(nombre, email, password):
+        # Verificar si el email ya existe
+        usuario_existente = Usuario.query.filter_by(email=email).first()
+
+        if usuario_existente:
+            return None, "El correo electrónico ya está registrado."
+
+        # Crear el usuario
+        usuario = Usuario(
+            nombre=nombre,
+            email=email
+        )
+
+        usuario.set_password(password)
+
+        db.session.add(usuario)
+        db.session.commit()
+
+        return usuario, None
