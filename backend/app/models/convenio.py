5b0164ea8ce5099154e7c6a6bb236a656c9f3487
Julieta <julieta_carmona_99@hotmail.com>
2026-07-27 22:58:23 -0300

    creación del modelo Convenio y migración

---
+++ backend/app/models/convenio.py
@@ -0,0 +1,29 @@
+from app.extensions import db
+
+
+class Convenio(db.Model):
+    __tablename__ = "convenios"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    titulo = db.Column(db.String(200), nullable=False)
+
+    descripcion = db.Column(db.Text)
+
+    fecha_firma = db.Column(db.Date)
+
+    estado = db.Column(
+        db.String(50),
+        default="En negociación"
+    )
+
+    usuario_id = db.Column(
+        db.Integer,
+        db.ForeignKey("usuarios.id"),
+        nullable=False
+    )
+    
+    usuario = db.relationship(
+    "Usuario",
+    backref="convenios"
+    )
