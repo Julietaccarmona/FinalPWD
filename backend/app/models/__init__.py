1e437999e261656354651e79f8caa07169667f9a
Julieta <julieta_carmona_99@hotmail.com>
2026-07-28 21:05:14 -0300

    creación del modelo TipoConvenio y migración

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from .usuario import Usuario
 from .convenio import Convenio
 from .pais import Pais
 from .actor import Actor
+from .tipo_convenio import TipoConvenio
---
+++ backend/app/models/tipo_convenio.py
@@ -0,0 +1,19 @@
+from app.extensions import db
+
+
+class TipoConvenio(db.Model):
+    __tablename__ = "tipos_convenio"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    nombre = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    descripcion = db.Column(
+        db.Text
+    )
